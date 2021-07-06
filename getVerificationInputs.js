a981b7b8e1e19def3f01922372e954c684141198
simpdrew <69509809+simpdrew@users.noreply.github.com>
2021-07-06 14:35:44 -0700

    Create getVerificationInputs.js

---
+++ getVerificationInputs.js
@@ -0,0 +1,22 @@
+// Dependencies
+var parser = require('cheerio')
+
+// Define
+exports.func = function(args) {
+	var $ = args.selector
+	if (!$) {
+		$ = parser.load(args.html)
+	}
+	var inputs = {}
+	var find = [
+		'__VIEWSTATE',
+		'__VIEWSTATEGENERATOR',
+		'__EVENTVALIDATION',
+		'__RequestVerificationToken'
+	]
+	for (var i = 0; i < find.length; i++) {
+		var get = find[i]
+		inputs[get] = $('input[name=' + get + ']').val()
+	}
+	return inputs
+}
